defe96cb113f42abe05a4e20ba5cd55bad8c8730
Elliot Olds <elliotolds@gmail.com>
2020-10-10 01:42:52 -0700

    Create regress_builtins.py
    try using pyTorch built in functions

---
+++ regress_builtins.py
@@ -0,0 +1,52 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+inputs = np.array([1,2,3,4,5,6,7,8,9,10], dtype='float32')
+targets = np.array([1,1.111374373,1.119168756,8.898077878,6.425522226,23.80948737,25.21115499,2.421243748,10.8532073,4.14585736], dtype='float32')
+
+print(inputs)
+print(targets)
+
+# Convert inputs and targets to tensors
+inputs = torch.from_numpy(inputs)
+targets = torch.from_numpy(targets)
+print(inputs)
+print(targets)
+
+# Import tensor dataset & data loader
+from torch.utils.data import TensorDataset, DataLoader
+
+# Define dataset
+train_ds = TensorDataset(inputs, targets)
+
+# Define data loader
+batch_size = 5
+train_dl = DataLoader(train_ds, batch_size, shuffle=True)
+#print(next(iter(train_dl)))
+
+# Define model
+model = nn.Linear(1, 1)
+print("weight and bias")
+print(model.weight)
+print(model.bias)
+
+# Define optimizer
+opt = torch.optim.SGD(model.parameters(), lr=1e-5)
+
+# Define loss function
+loss_fn = F.mse_loss
+
+print(model(inputs))
+
+"""
+loss = loss_fn(model(inputs), targets)
+print("\nLoss is:")
+print(loss)
+"""
